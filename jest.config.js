ef025ddc03874231cda324c0c6e0cf1aadba0ff0
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2020-06-13 18:42:44 -0400

    Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,25 @@
+process.env.VUE_CLI_BABEL_TARGET_NODE = true;
+process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  testURL: 'http://localhost/',
+  // testEnvironment: 'jest-environment-jsdom-global',
+  snapshotSerializers: [
+    '<rootDir>/node_modules/jest-serializer-vue-tjw'
+  ],
+  // setupFilesAfterEnv: ['<rootDir>/tests/unit/setup'],
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**/node_modules/**'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.test.js'
+  ],
+  moduleNameMapper: {
+    '^@@/(.*)$': '<rootDir>/tests/unit/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
