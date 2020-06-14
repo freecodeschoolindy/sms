c4381909b78375e9208e2bbfcf86cf58bf1cb897
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2020-06-14 18:31:25 -0400

    Router test

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,18 +5,7 @@ const helpers = {
     'organizer',
     'volunteer',
     'admin'
-  ],
-  generateTempId: function () {
-    let prefix = 'temp';
-    let random = Math.random().toString(36).substr(2, 9);
-    let date = Date.now();
-
-    return [
-      prefix,
-      random,
-      date
-    ].join('_');
-  }
+  ]
 };
 
 export default helpers;
--- tests/unit/src/router/router.test.js
+++ tests/unit/src/router/router.test.js
@@ -1,12 +1,79 @@
-import { routes } from '@/router/index.js';
+import store from '@/store/index.js';
+import { routes, loadUser, checkRoutePermissions } from '@/router/index.js';
 
 describe('Router', () => {
-  test('All routes have meta.group', () => {
-    let validRoutes = routes.filter((route) => {
-      return route?.meta?.groups.length;
+  beforeEach(() => {
+    store.state.currentUser = {};
+  });
+
+  describe('Routes', () => {
+    test('All routes have meta.group', () => {
+      let validRoutes = routes.filter((route) => {
+        return route?.meta?.groups.length;
+      });
+
+      expect(validRoutes.length)
+        .toEqual(routes.length);
+    });
+  });
+
+  describe('loadUser', () => {
+    const to = {};
+    const from = {};
+    const next = jest.fn();
+
+    test('Stores data in the state', () => {
+      loadUser(to, from, next);
+
+      expect(store.state.currentUser)
+        .toEqual({
+          firstName: 'Jane',
+          lastName: 'Doe',
+          preferredName: 'Janey',
+          email: 'janedoe@example.com',
+          groups: 'student',
+          id: 20
+        });
     });
 
-    expect(validRoutes.length)
-      .toEqual(routes.length);
+    test('Skips network request if currentUser is already loaded', () => {
+      store.state.currentUser = { id: 3 };
+      const spyCommit = jest.spyOn(store, 'commit');
+
+      loadUser(to, from, next);
+
+      expect(spyCommit)
+        .not.toHaveBeenCalled();
+    });
+  });
+
+  describe('checkRoutePermissions', () => {
+    function setState (groups) {
+      store.state.currentUser = { groups };
+    }
+    const to = {
+      meta: {
+        groups: ['student']
+      }
+    };
+    const next = jest.fn();
+
+    test('Correct permissions', () => {
+      setState('student');
+
+      checkRoutePermissions(to, null, next);
+
+      expect(next)
+        .toHaveBeenCalledWith();
+    });
+
+    test('Incorrect permissions', () => {
+      setState('unassigned');
+
+      checkRoutePermissions(to, null, next);
+
+      expect(next)
+        .toHaveBeenCalledWith('/access-denied');
+    });
   });
 });
