9bc582d8c462291a1a563bec85d475ddb08fb80c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2020-07-19 09:22:13 -0400

    linting

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -6,13 +6,20 @@
       </div>
 
       <div class="cell">
-        <input type="text" v-model="github" placeholder="Github Username">
+        <label>
+          Github Username:
+          <input v-model="github" type="text" placeholder="Github Username" />
+        </label>
       </div>
       <div class="cell">
-        <input type="text" v-model="email" placeholder="Email">
+        <label>
+          Email:
+          <input v-model="email" type="text" placeholder="Email" />
+        </label>
       </div>
       <div class="cell">
-        <label>Select Student Status
+        <label>
+          Select Student Status:
           <select v-model="studentStatus">
             <option value="current">I am currently a student</option>
             <option value="former">I am a former student</option>
@@ -22,16 +29,25 @@
         </label>
       </div>
       <div class="cell">
-        <input type="text" v-model="firstName" placeholder="First Name">
+        <label>
+          First Name:
+          <input v-model="firstName" type="text" placeholder="First Name" />
+        </label>
       </div>
       <div class="cell">
-        <input type="text" v-model="lastName" placeholder="Last Name">
+        <label>
+          Last Name:
+          <input v-model="lastName" type="text" placeholder="Last Name" />
+        </label>
       </div>
       <div class="cell">
-        <input type="text" v-model="preferredName" placeholder="Preferred Name">
+        <label>
+          Preferred Name:
+          <input v-model="preferredName" type="text" placeholder="Preferred Name" />
+        </label>
       </div>
       <div class="cell">
-        <button @click="registerUser" class="button">Submit</button>
+        <button class="button" @click="registerUser">Submit</button>
       </div>
     </div>
   </form>
@@ -39,7 +55,7 @@
 
 <script>
 export default {
-  data() {
+  data: function () {
     return {
       github: '',
       email: '',
@@ -47,12 +63,12 @@ export default {
       lastName: '',
       preferredName: '',
       studentStatus: ''
-    }
+    };
   },
   methods: {
-    registerUser() {
+    registerUser: function () {
 
     }
   }
-}
+};
 </script>
